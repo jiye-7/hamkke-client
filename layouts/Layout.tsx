ffa36f97fc2d58ec0de4f20eb9fc888d42f40629
jiye-7 <62678492+jiye-7@users.noreply.github.com>
2024-08-22 19:20:38 +0900

    임시 nav 적용

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -1,12 +1,22 @@
-import { ReactNode } from "react";
+import Link from 'next/link';
+import { ReactNode } from 'react';
 
+// TODO: Navigation, Footer 등
 const Layout = ({ children }: { children: ReactNode }) => {
-  return (
-    <div>
-      {children}
-      <span>Test Layouts</span>
-    </div>
-  );
+	return (
+		<div className="bg-slate-50 min-h-screen">
+			<nav className="flex justify-end mt-6">
+				<Link href="/users/login">
+					<span className="font-bold text-gray-700 mr-5">Login</span>
+				</Link>
+				<Link href="/users/signup">
+					<span className="font-bold text-gray-700 mr-5">SignUp</span>
+				</Link>
+			</nav>
+			{children}
+			<span className="bg-red-500">Test Layouts</span>
+		</div>
+	);
 };
 
 export default Layout;
